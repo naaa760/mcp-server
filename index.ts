ca94ebfaa85c497b53cc389561ff0ef082bb16cd
nehhhhha6 <n0370525@gmail.com>
2025-04-29 17:05:21 +0530

    update

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import { KiteConnect } from "kiteconnect";
 
 const apiKey = "kbyvnwxwh8uou3im";
 const apiSecret = "5jwa0psompa54k2m5zkopqitx1lhn67i";
-const requestToken = "your_request_token";
+const requestToken = "XQKWf4xnzqD0A9FkrCMdH8jy6wOmxyyC";
 
 const kc = new KiteConnect({ api_key: apiKey });
+
+console.log(kc.getLoginURL());
 
 async function init() {
   try {
